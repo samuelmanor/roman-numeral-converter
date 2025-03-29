48d62d5dafc4ef3775d08bff331e21cc8affa018
sam <manor@hey.com>
2025-03-28 21:51:39 -0400

    add winston to log backend requests

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,47 @@
 const express = require("express");
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { toRoman } from "./toRoman";
+const winston = require("winston");
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
 const app = express();
+
+// Logs incoming requests to the console
+const logger = winston.createLogger({
+  level: "info",
+  format: combine(
+    colorize({ all: true }),
+    timestamp(),
+    align(),
+    printf(
+      (info: {
+        timestamp: string;
+        level: string;
+        message: string;
+        method: string;
+        path: string;
+        query: { query: string };
+      }) =>
+        `[${info.timestamp}] ${info.level}: ${info.message} (${info.method} ${info.path} query: ${info.query.query})`
+    )
+  ),
+  defaultMeta: { service: "roman-numeral-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console(),
+  ],
+});
+
+// Middleware to log requests
+app.use((request: Request, response: Response, next: NextFunction) => {
+  logger.info(`Incoming request`, {
+    method: request.method,
+    path: request.path,
+    query: request.query,
+  });
+  next();
+});
 
 app.get("/romannumeral", (request: Request, response: Response) => {
   const query = request.query.query;
