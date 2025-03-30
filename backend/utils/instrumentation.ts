4d81aab955273bcad1ab3dae675548c9e24ba79c
sam <manor@hey.com>
2025-03-30 14:26:05 -0400

    add graceful shutdown handling

--- backend/utils/instrumentation.ts
+++ backend/utils/instrumentation.ts
@@ -17,3 +17,13 @@ const sdk = new NodeSDK({
 });
 
 sdk.start(); // Starts the OpenTelemetry SDK, enabling tracing and metrics collection
+
+// Handle shutdown gracefully
+// This ensures that all pending spans and metrics are flushed before the process exits
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => console.log("Tracing terminated"))
+    .catch((error) => console.log("Error terminating tracing", error))
+    .finally(() => process.exit(0));
+});
